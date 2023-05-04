b672cd3f7792a627415af1303fbd30717907e2a6
unknown <muhammadehti97@gmail.com>
2023-05-04 08:52:44 +0500

    setup gh-pages

--- src/component/Api.js
+++ src/component/Api.js
@@ -3,17 +3,17 @@
 export const questions = [
     {
         id:1,
-        question: "what is dom ?",
-        answer: "dom is dom"
+        question: "what is DOM ?",
+        answer: "The HTML DOM is a standard for how to get, change, add, or delete HTML elements"
     },
     {
         id:2,
-        question: "whats is js ?",
-        answer: "js is java script"
+        question: "whats is javaScript ?",
+        answer: "JavaScript is a scripting language that enables you to create dynamically updating content, control multimedia, animate images, and pretty much everything else. (Okay, not everything, but it is amazing what you can achieve with a few lines of JavaScript code.)"
     },
     {
         id:3,
-        question: "what is css ?",
-        answer : "css is cascading style sheet"
+        question: "what is CSS ?",
+        answer : "Cascading Style Sheets, fondly referred to as CSS, is a simple design language intended to simplify the process of making web pages presentable. CSS handles the look and feel part of a web page. Using CSS, you can control the color of the text, the style of fonts, the spacing between paragraphs, how columns are sized and laid out, what background images or colors are used, layout designs,variations in display for different devices and screen sizes as well as a variety of other effects."
     }
 ]
